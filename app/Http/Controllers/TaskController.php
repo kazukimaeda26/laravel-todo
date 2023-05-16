0bfaf338db869b122a76c5f25c2dd82529a4d2d9
Kazuki Maeda <maedakazuki26@gmail.com>
2023-05-16 11:28:28 +0900

    タスクの一覧表示

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
         $folders = Folder::all();
+
+        $current_folder = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
         return view('tasks/index',['folders' => $folders,
         'current_folder_id' => $id,
+        'tasks' => $tasks,
     ]);
     }
 }
