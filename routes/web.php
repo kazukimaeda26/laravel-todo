6c38f4d51d0958d881304a020cf0af7b59aca098
Kazuki Maeda <maedakazuki26@gmail.com>
2023-05-16 11:52:30 +0900

    ビューの作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\FolderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
 
