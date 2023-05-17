38fea775aff58565aab941422755cb5edc02584b
Kazuki Maeda <maedakazuki26@gmail.com>
2023-05-17 10:08:20 +0900

    タスク作成ページのビュー

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,14 +11,19 @@ class TaskController extends Controller
     public function index(int $id)
     {
         $folders = Folder::all();
-
         $current_folder = Folder::find($id);
-
         $tasks = $current_folder->tasks()->get();
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+            'tasks' => $tasks,
+        ]);
+    }
 
-        return view('tasks/index',['folders' => $folders,
-        'current_folder_id' => $id,
-        'tasks' => $tasks,
-    ]);
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
     }
 }
